0063636891f747c144e598e2e1a25af8c9983a38
Francesco di Biase <fran.dibiase@gmail.com>
2025-09-10 23:17:58 +0200

    starting project

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import {Server} from "socket.io";
+import http from "http";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+});
+
+server.listen(3000, () => {
+    console.log("server is running on port 3000");
+});
